80e0211713518f1bc2bf7111ee89f653ad6223d3
Weiqiu You <rachel_you@outlook.com>
2019-06-24 17:44:05 -0400

    Debug probe transformer

--- models/utils.py
+++ models/utils.py
@@ -407,7 +407,7 @@ def probe(attn_weights):
 
     argmax_i = topi.squeeze(-1)
     argmax_i_size = argmax_i.size()
-    small_argmax_i_size = argmax_i_size
+    small_argmax_i_size = list(argmax_i_size)
     small_argmax_i_size[:-1] = 1
     original_i = torch.arange(argmax_i.size()[-1]).view(small_argmax_i_size).expand(argmax_i_size)
     argmax_distances = argmax_i - original_i
