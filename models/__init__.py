5adc4d787aa610bef49edfd8b06585b926994c63
Weiqiu You <rachel_you@outlook.com>
2019-07-01 15:17:41 -0400

    Add and debug new attention

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,11 @@ Initialize the models module
 from models.transformer import Transformer
 from models.parse import ParseTransformer
 from models.probe_transformer import ProbeTransformer
+from models.new_transformer import NewTransformer
 
 MODELS = {
     'transformer': Transformer,
     'parse_transformer': ParseTransformer,
-    'probe_transformer': ProbeTransformer
+    'probe_transformer': ProbeTransformer,
+    'new_transformer': NewTransformer
 }
