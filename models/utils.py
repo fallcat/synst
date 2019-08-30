2fc1a3ee45ef7276fec4c8e4b03d9561b3f2a871
fallcat <rachel_you@outlook.com>
2019-08-30 15:27:24 -0400

    debug probe new

--- models/utils.py
+++ models/utils.py
@@ -539,7 +539,7 @@ def save_attention(input_sentence, output_words, attentions, file_path):
     # Set up axes
     ax.set_xticklabels([''] + input_sentence.split(' ') +
                        ['<EOS>'], rotation=90)
-    ax.set_yticklabels([''] + output_words)
+    ax.set_yticklabels([''] + output_words.split(' '))
 
     # Show label at every tick
     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
