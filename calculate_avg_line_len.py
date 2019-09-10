48a96c39f209fb85993c8a4b1b9b8b5e4e68db84
fallcat <rachel_you@outlook.com>
2019-09-10 14:19:07 -0400

    Add calculate micro average

---
+++ calculate_avg_line_len.py
@@ -0,0 +1,10 @@
+with open('/mnt/nfs/work1/miyyer/wyou/iwslt/train.tok.bpe.32000.en') as file1:
+    with open('/mnt/nfs/work1/miyyer/wyou/iwslt/train.tok.bpe.32000.de') as file2:
+        count = 0
+        ratio = 0.0
+        for x, y in zip(file1, file2):
+            x_list = x.split()
+            y_list = y.split()
+            ratio += len(x_list) / float(len(y_list))
+            count += 1
+        print(ratio / count)
