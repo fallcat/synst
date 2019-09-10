3fc4101cfc95672fa729eb20a6e156d4caa27289
fallcat <rachel_you@outlook.com>
2019-09-10 13:41:54 -0400

    Debug source attention

--- models/new_attention.py
+++ models/new_attention.py
@@ -355,7 +355,7 @@ class NewAttention(nn.Module):
 
         # torch.set_printoptions(profile='full')
         print("values", values)
-        print("values shape" values.shape)
+        print("values shape", values.shape)
         print("attn_weights", attn_weights)
         print("attn_weights shape", attn_weights.shape)
         print("attended", attended)
