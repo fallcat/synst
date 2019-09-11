e159a5816dbf6f584408ca91d39be4468cdef5db
fallcat <rachel_you@outlook.com>
2019-09-10 22:57:46 -0400

    Remove print, fix source eval

--- models/new_transformer.py
+++ models/new_transformer.py
@@ -189,7 +189,7 @@ class TransformerDecoderLayer(nn.Module):
         if self.causal and cache is not None:
             kwargs['num_queries'] = self.span
             kwargs['decoder_position'] = decoder_position
-            print("kwargs['decoder_position']", kwargs['decoder_position'])
+            # print("kwargs['decoder_position']", kwargs['decoder_position'])
 
         # print("decoder source attention")
 
