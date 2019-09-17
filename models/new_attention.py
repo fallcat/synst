89c36835a63bbfb7b930739dd55631f3be05fb3c
fallcat <rachel_you@outlook.com>
2019-09-17 17:16:53 -0400

    Debug Oracle Eval

--- models/new_attention.py
+++ models/new_attention.py
@@ -106,6 +106,8 @@ class NewAttention(nn.Module):
         print("target_lens", target_lens)
         queries_shape = queries.shape
         values_shape = values.shape
+        print("queries_shape", queries_shape)
+        print("values_shape", values_shape)
         # print("self.word_count_ratio", self.word_count_ratio)
 
         # By this point the values, keys, and queries all have B * H as their first dimension
@@ -363,8 +365,8 @@ class NewAttention(nn.Module):
         # torch.set_printoptions(profile='full')
         # print("values", values)
         # print("values shape", values.shape)
-        # print("attn_weights", attn_weights)
-        # print("attn_weights shape", attn_weights.shape)
+        print("attn_weights", attn_weights)
+        print("attn_weights shape", attn_weights.shape)
         # print("attended", attended)
         # print("attended shape", attended.shape)
 
