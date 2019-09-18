053f06f0d4ada2e51939041ff3515915e159aa39
fallcat <rachel_you@outlook.com>
2019-09-18 00:37:19 -0400

    Add align stats

--- align_stats.py
+++ align_stats.py
@@ -1,10 +1,14 @@
+import pickle
+import numpy as np
+
 split_portion = 50
 
 with open('../iwslt/train.tok.en', 'rt') as file_en:
     with open('../iwslt/train.tok.de', 'rt') as file_de:
         with open('../iwslt/forward.align', 'rt') as file_fa:
-            with open('../iwslt/forward.align.stats', 'wt') as file_fas:
+            with open('../iwslt/forward.align.pickle', 'wb') as file_fas:
                 final_count = {}
+                stats = {}
                 for x, y, z in zip(file_en, file_de, file_fa):
                     z_dict = {}
                     len_x = len(x.split())
@@ -22,4 +26,11 @@ with open('../iwslt/train.tok.en', 'rt') as file_en:
                                 final_count[y_word] = {}
                                 final_count[y_word][i] = [z_dict[i]]
                 for key in final_count:
-                    file_fas.write(key + ':' + str(final_count[key]) + '\n')
+                    stats[key] = {}
+                    for num in final_count[key]:
+                        mean = np.mean(final_count[key][num])
+                        std = np.std(final_count[key][num])
+                        stats[key][num] = {}
+                        stats[key][num]['mean'] = mean
+                        stats[key][num]['std'] = std
+                pickle.dump(stats, file_fas)
