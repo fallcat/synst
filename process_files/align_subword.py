3e6a4a2814ab666e214b29d1a27345b428539fda
fallcat <rachel_you@outlook.com>
2019-09-23 15:44:00 -0400

    Debug align_subword

--- process_files/align_subword.py
+++ process_files/align_subword.py
@@ -3,9 +3,9 @@ with open('../iwslt/train.bpe.idx.mapping.en', 'rt') as source_file:
         with open('../iwslt/forward.align', 'rt') as align_file:
             with open('../iwslt/forward.subword.align', 'wt') as output_file:
                 for source_line, target_line, align_line in zip(source_file, target_file, align_file):
-                    source_list = [w.split(':')[1].split(',') for w in source_line.split()]
-                    target_list = [w.split(':')[1].split(',') for w in target_line.split()]
-                    align_list = [w.split('-') for w in align_line.split()]
+                    source_list = [[int(x) for x in w.split(':')[1].split(',')] for w in source_line.split()]
+                    target_list = [[int(x) for x in w.split(':')[1].split(',')] for w in target_line.split()]
+                    align_list = [[int(x) for x in w.split('-')] for w in align_line.split()]
                     print(align_list)
                     output_list = []
                     for item in align_list:
@@ -14,7 +14,7 @@ with open('../iwslt/train.bpe.idx.mapping.en', 'rt') as source_file:
                         target = item[1]
                         new_source = source_list[source]
                         new_target = target_list[target]
-                        if len(new_source) == 1 and len(new_target == 1):
+                        if len(new_source) == 1 and len(new_target) == 1:
                             output_list.append([new_source[0], new_target[0]])
                         else:
                             base = new_source[0] - 0.5
