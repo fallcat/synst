8fdb9f99f91ee553c7e0f0fd2dce68cf8d913325
fallcat <rachel_you@outlook.com>
2019-09-23 15:51:56 -0400

    Debug align_subword

--- process_files/align_subword.py
+++ process_files/align_subword.py
@@ -17,6 +17,10 @@ with open('../iwslt/train.bpe.idx.mapping.en', 'rt') as source_file:
                         else:
                             base = new_source[0] - 0.5
                             try:
+                                for i, w in enumerate(new_target):
+                                    print("(new_source[-1] - new_source[0] + 1)", (new_source[-1] - new_source[0] + 1))
+                                    print("(w - base)", (w - base))
+                                    print("(new_target[-1] + 0.5 - base)", (new_target[-1] + 0.5 - base))
                                 output_list.extend([[base + (new_source[-1] - new_source[0] + 1) *
                                                     (w - base) / (new_target[-1] + 0.5 - base), w]
                                                     for i, w in enumerate(new_target)])
