b2ff7c800c23fab0dce67006e9dc9d8fd01569ab
fallcat <rachel_you@outlook.com>
2019-09-22 20:32:26 -0400

    Debug align_stats to put each number in the correct bin

--- align_stats.py
+++ align_stats.py
@@ -20,7 +20,7 @@ with open('../iwslt/train.tok.en', 'rt') as file_en:
                         a, b = int(a), int(b)
                         key = math.ceil((b + 0.5) / len_y * split_portion)  # round((b + 1) / len_x * split_portion) - 1
                         if key in z_dict:
-                            z_dict[key].append(a - b * WORD_COUNT[0])  # round((int(a) + 1) / len_x * split_portion) - 1
+                            z_dict[key].append(a - b * 1.0360595565014956)  # round((int(a) + 1) / len_x * split_portion) - 1
                         else:
                             z_dict = [a - b * WORD_COUNT[0]]
                     for i, y_word in enumerate(y.split()):
