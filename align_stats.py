4e96cd6365dcb0bdea55c808522a588d649a69e2
fallcat <rachel_you@outlook.com>
2019-09-22 20:30:40 -0400

    Debug align_stats to put each number in the correct bin

--- align_stats.py
+++ align_stats.py
@@ -1,24 +1,28 @@
 import pickle
+import math
 import numpy as np
 
 split_portion = 4
+WORD_COUNT = (1.0360595565014956, 1)
 
 with open('../iwslt/train.tok.en', 'rt') as file_en:
     with open('../iwslt/train.tok.de', 'rt') as file_de:
         with open('../iwslt/forward.align', 'rt') as file_fa:
-            with open('../iwslt/forward.align.pickle', 'wb') as file_fas:
+            with open('../iwslt/forward.align.4.pickle', 'wb') as file_fas:
                 final_count = {}
                 stats = {}
                 for x, y, z in zip(file_en, file_de, file_fa):
                     z_dict = {}
                     len_x = len(x.split())
+                    len_y = len(y.split())
                     for z_element in z.split():
                         a, b = z_element.split('-')
-                        key = round((int(b) + 1) / len_x * split_portion) - 1
+                        a, b = int(a), int(b)
+                        key = math.ceil((b + 0.5) / len_y * split_portion)  # round((b + 1) / len_x * split_portion) - 1
                         if key in z_dict:
-                            z_dict[key].append(a - b)  # round((int(a) + 1) / len_x * split_portion) - 1
+                            z_dict[key].append(a - b * WORD_COUNT[0])  # round((int(a) + 1) / len_x * split_portion) - 1
                         else:
-                            z_dict = [a - b]
+                            z_dict = [a - b * WORD_COUNT[0]]
                     for i, y_word in enumerate(y.split()):
                         new_i = round((int(i) + 1) / len_x * split_portion) - 1
                         if new_i in z_dict:
