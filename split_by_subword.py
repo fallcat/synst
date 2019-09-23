986db7d8b114b107c9f96bddd438983d1777bae6
fallcat <rachel_you@outlook.com>
2019-09-23 14:59:45 -0400

    Add split by subword

---
+++ split_by_subword.py
@@ -0,0 +1,13 @@
+with open('../iwslt/train.tok.bpe.32000.en', 'rt') as input_file:
+    with open('../iwslt/train.bpe.idx.mapping.en', 'wt') as output_file:
+        for line in input_file.readlines():
+            line_list = line.split()
+            mapping = []
+            new_subwords = []
+            for i, w in enumerate(line_list):
+                new_subwords.append(i)
+                if w[-2:] != '@@':
+                    mapping.append(new_subwords)
+                    new_subwords = []
+            s = ' '.join([str(i)+':'+','.join([str(n) for n in subwords]) for i, subwords in enumerate(mapping)])
+            output_file.write(s + '\n')
