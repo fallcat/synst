ef206c25f713e2d450b83ed45309767add7e90ba
fallcat <rachel_you@outlook.com>
2019-10-04 00:24:51 -0400

    Debug probe new translate

--- actions/probe_new_translate.py
+++ actions/probe_new_translate.py
@@ -79,6 +79,7 @@ class ProbeNewTranslator(object):
             self.model.eval()
             ordered_outputs = []
             for batch in batches:
+                print("in probe new translate")
                 # run the data through the model
                 batches.set_description_str(get_description())
                 sequences, attn_weights_tensors_dict = self.translator.translate(batch)
