9f231d03a63b5d091e7dcc8bd6ac3224d1b55633
fallcat <rachel_you@outlook.com>
2019-10-04 00:48:30 -0400

    Debug probe new translate

--- models/probe_new_attention.py
+++ models/probe_new_attention.py
@@ -435,8 +435,8 @@ class ProbeNewAttention(nn.Module):
         # torch.set_printoptions(profile='full')
         # print("values", values)
         # print("values shape", values.shape)
-        print("attn_weights", attn_weights)
-        print("attn_weights shape", attn_weights.shape)
+        # print("attn_weights", attn_weights)
+        # print("attn_weights shape", attn_weights.shape)
         # print("attended", attended)
         # print("attended shape", attended.shape)
 
