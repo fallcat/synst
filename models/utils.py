ad3108d7e43a1933705346b6a152a3e380854fa4
fallcat <rachel_you@outlook.com>
2019-10-04 00:22:51 -0400

    Debug probe new translate

--- models/utils.py
+++ models/utils.py
@@ -536,6 +536,10 @@ def save_attention(input_sentence, output_words, attentions, file_path):
     fig.colorbar(cax)
 
     # Set up axes
+    print("input_sentence", input_sentence)
+    print("input_sentence len", len(input_sentence.split(' ')))
+    print("output_words", output_words)
+    print("output_words len", len(output_words.split(' ')))
     ax.set_xticklabels([''] + input_sentence.split(' ') +
                        ['<EOS>'], rotation=90)
     ax.set_yticklabels([''] + output_words.split(' '))
