7371e197c4337ef42b4e6f994ecbbd4bf6f6ef87
SimengSun <simsun.0131@gmail.com>
2019-10-08 15:30:02 -0400

    debug

--- data/text.py
+++ data/text.py
@@ -116,15 +116,15 @@ class TextDataset(Dataset):
 
         def sorter(examples, key='input'):
             ''' Sort the list of examples based on the length of the sequence for the given key '''
-            print(examples)
-            print(examples[0])
-            print(examples[1])
-            pdb.set_trace()
+            #print(examples)
+            #print(examples[0])
+            #print(examples[1])
+            #pdb.set_trace()
             try:             
                 ret = sorted(examples, key=lambda x: len(x[1][key]), reverse=True)
             except:
                 print(examples)
-                pdb.set_trace()
+                #pdb.set_trace()
             return ret
 
         if any(
