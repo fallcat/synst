6b51eada3614e51564a2496e137f5331b89604a4
SimengSun <simsun.0131@gmail.com>
2019-10-11 14:58:02 -0400

    pdb

--- models/new_attention.py
+++ models/new_attention.py
@@ -133,7 +133,7 @@ class NewAttention(nn.Module):
 
         for i, attn_config_i in enumerate([self.attn_type, self.attn_position, self.attn_param, self.attn_displacement]):
             if type(attn_config_i) is list:
-                pdb.set_trace()
+                #pdb.set_trace()
                 if len(attn_config_i) == 1:
                     attn_configs.append(attn_config_i[0])
                 elif len(attn_config_i) == self.num_heads:
