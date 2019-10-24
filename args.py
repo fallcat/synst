f51fec7c08cd2dd00bce444df6e562cd537e4091
SimengSun <simsun.0131@gmail.com>
2019-10-23 22:46:20 -0400

    comet exp name

--- args.py
+++ args.py
@@ -1221,7 +1221,7 @@ Commit your changes first, then try again.''')
 
     args.experiment = experiment_type(
         *experiment_args,
-        project_name='probe-transformer',
+        project_name='transformer-attn',
         workspace='umass-nlp',
         disabled=not args.track,
         auto_metric_logging=False,
