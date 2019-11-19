50367f3b498fe3fe5a306f7b8179570f1d4c94f1
fallcat <rachel_you@outlook.com>
2019-11-19 16:09:57 -0500

    Debug source CNN

--- models/new_attention.py
+++ models/new_attention.py
@@ -315,7 +315,7 @@ class NewAttention(nn.Module):
                     if values_shape[1] >= queries_shape[1]:
                         print("greater")
                         if self.word_count_ratio != 1:
-                            indices_q = torch.round(torch.arange(queries_shape[1]) * self.word_count_ratio).to(dtype=torch.long)
+                            indices_q = torch.round(torch.arange(queries_shape[1], device=values.get_device()) * self.word_count_ratio).to(dtype=torch.long)
                             conv_attended = attended[:, indices_q]
                         else:
                             conv_attended = attended[:, :queries_shape[1]]
