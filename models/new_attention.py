ceb45e7d06ae0f8064f953eb54a82863dda29d93
SimengSun <simsun.0131@gmail.com>
2019-11-21 23:34:41 -0500

    index

--- models/new_attention.py
+++ models/new_attention.py
@@ -227,7 +227,6 @@ class NewAttention(nn.Module):
 
         # simple indexing - fix window size 1
         if self.attn_indexing:
-            print("using simple indexing")
             max_last_index = -1
             if type(attn_position) is list and 'last' in attn_position or attn_position == 'last':
                 if input_lens is not None:
@@ -254,7 +253,7 @@ class NewAttention(nn.Module):
             values = values.view(batch_size, self.num_heads, values_shape[1], values_shape[2]) # bs x num_heads x vlen x proj_dim
 
             # append zeros at vlen+1
-            max_padding = 1 + max(attn_displacement)
+            max_padding = max(attn_displacement)
             values = F.pad(values, (0, 0, max_padding, max_padding), "constant", 0)
             
             if decoder_position == -1:
@@ -263,6 +262,7 @@ class NewAttention(nn.Module):
                 indices_last = torch.round(torch.arange(decoder_position + 1).type_as(values) * self.word_count_ratio).long()
 
             indices_q = torch.round(torch.arange(queries_shape[1]).view(-1, 1).type_as(values) * self.word_count_ratio).long()
+            indices_q[indices_q >= values_shape[1]] = values_shape[1] - 1
 
             attended = []
             for i, p, in enumerate(attn_position):
