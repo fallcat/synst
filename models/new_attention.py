3e4b9228b2d35c9a2f5424d266ff910f735ea2b4
SimengSun <simsun.0131@gmail.com>
2019-11-26 11:41:14 -0500

    decoder position

--- models/new_attention.py
+++ models/new_attention.py
@@ -274,8 +274,13 @@ class NewAttention(nn.Module):
                         print("unknown position")
                         exit(-1)
 
-                # bs x nh x qlen x proj_dim
-                attended_indices = attended_indices.expand(batch_size, self.num_heads, queries_shape[1], self.projection_dim)
+                if decoder_position == -1:
+                    # bs x nh x qlen x proj_dim
+                    attended_indices = attended_indices.expand(batch_size, self.num_heads, queries_shape[1], self.projection_dim)
+
+                else:
+                    # bs x nh x qlen x proj_dim
+                    attended_indices = attended_indices.expand(batch_size, self.num_heads, decoder_position+1, self.projection_dim)
             
             # return
             return torch.gather(values, 2, attended_indices).transpose(2,1).contiguous().view(batch_size, -1, self.num_heads * self.projection_dim)
