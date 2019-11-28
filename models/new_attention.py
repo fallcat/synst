fccd47d9d7d6fafb2fdc9d305feba55f543795b8
SimengSun <simsun.0131@gmail.com>
2019-11-28 09:08:28 -0500

    fix padding

--- models/new_attention.py
+++ models/new_attention.py
@@ -243,21 +243,24 @@ class NewAttention(nn.Module):
             values = values.view(batch_size, self.num_heads, values_shape[1], values_shape[2]) 
 
             max_padding = max(attn_displacement)
-            values = F.pad(values, (0, 0, max_padding, max_padding), "constant", 0)
+            max_query_len = queries_shape[1] if decoder_position == -1 else decoder_position + 1
+            more_padding = round((max_query_len - 1) * self.word_count_ratio) + 1 - values_shape[1]
+            if more_padding > 0
+                values = F.pad(values, (0, 0, max_padding, max_padding + more_padding), "constant", 0)
+            else:
+                values = F.pad(values, (0, 0, max_padding, max_padding), "constant", 0)
             
             with torch.no_grad():
+                max_query_len = queries_shape[1] if decoder_position == -1 else decoder_position + 1
 
                 if decoder_position == -1:
                     indices_q = torch.round(torch.arange(queries_shape[1]).view(-1, 1).type_as(values) * self.word_count_ratio).long()
-                    indices_q[indices_q >= values_shape[1]] = values_shape[1] - 1
                     attended_indices = torch.zeros(1, self.num_heads, queries_shape[1], 1).type_as(values).long() # 1 x num_heads x qlen x 1
 
                 else:
                     # pdb.set_trace()
                     indices_q = torch.round(torch.arange(decoder_position, decoder_position+1).view(-1, 1).type_as(values) * self.word_count_ratio).long()
                     attended_indices = torch.zeros(1, self.num_heads, 1, 1).type_as(values).long() # 1 x num_heads x 1 x 1
-                    indices_q[indices_q >= values_shape[1]] = values_shape[1] - 1
-                    
 
                 for i, p, in enumerate(attn_position):
                     if p == "center":
@@ -285,7 +288,9 @@ class NewAttention(nn.Module):
                     attended_indices = attended_indices.expand(batch_size, self.num_heads, 1, self.projection_dim)
             
             # return
+            # index_attended = torch.gather(values, 2, attended_indices).transpose(2,1).contiguous().view(batch_size, -1, self.num_heads * self.projection_dim)
             return torch.gather(values, 2, attended_indices).transpose(2,1).contiguous().view(batch_size, -1, self.num_heads * self.projection_dim)
+
 
         # If we are using learned attention, then just do it the same way as multi-headed attention
         if attn_type == 'learned' or learned:
