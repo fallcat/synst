ab77214270cf36e53b375aa4d8b6c2d43cd7daaa
SimengSun <simsun.0131@gmail.com>
2019-12-01 19:35:53 -0500

    indexing dec pos

--- models/new_attention.py
+++ models/new_attention.py
@@ -322,11 +322,10 @@ class NewAttention(nn.Module):
                     indices_q = torch.round(torch.arange(queries_shape[1]).type_as(values) * self.word_count_ratio).long()
 
                 else:
-                    # pdb.set_trace()
+                    pdb.set_trace()
                     indices_q = torch.round(torch.arange(decoder_position, decoder_position+1).type_as(values) * self.word_count_ratio).long()
 
                 attended = []
-                #pdb.set_trace()
                 for i, p, in enumerate(attn_position):
                     if p == "center":
                         attended.append(values[:, i, max_padding + indices_q])
