e3d2a987d9f0a20838bbfd2ae0133981a68b3ae2
fallcat <rachel_you@outlook.com>
2020-03-17 22:08:41 -0400

    Fix storing of raw_layermask

--- utils/beam_search.py
+++ utils/beam_search.py
@@ -224,16 +224,18 @@ class BeamSearchDecoder(object):
 
                 # expand raw_layermask
                 # assert len(raw_layermask) == len(beam_count)
-                if sum(beam_count) != raw_layermask.shape[0]:
-                    new_raw_layermask = []
-                    for eg, rtimes in zip(raw_layermask, beam_count):
-                        if rtimes == 0:
-                            continue
-                        new_raw_layermask.append(eg.repeat(rtimes, 1))
-                        #pdb.set_trace()
-                    new_raw_layermask = torch.cat(new_raw_layermask)
-                else:
-                    new_raw_layermask = raw_layermask
+                # if sum(beam_count) != raw_layermask.shape[0]:
+                #     new_raw_layermask = []
+                #     for eg, rtimes in zip(raw_layermask, beam_count):
+                #         if rtimes == 0:
+                #             continue
+                #         new_raw_layermask.append(eg.repeat(rtimes, 1))
+                #         #pdb.set_trace()
+                #     new_raw_layermask = torch.cat(new_raw_layermask)
+                # else:
+                #     new_raw_layermask = raw_layermask
+
+                new_raw_layermask = raw_layermask
 
                 chunks = [(encoded_batch, batch, new_raw_layermask)]
                 while chunks:
