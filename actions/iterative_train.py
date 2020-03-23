796bdac63c9aa430e34cfcefa51a031bb711deb5
SimengSun <simsun.0131@gmail.com>
2020-03-22 20:48:27 -0400

    epoch start ckpt

--- actions/iterative_train.py
+++ actions/iterative_train.py
@@ -348,7 +348,7 @@ class IterativeTrainer(object):
                             end_flag = True
                             break
 
-                        if self.is_best_checkpoint(val_losses) and epoch_i > epoch + 4:
+                        if self.is_best_checkpoint(val_losses) and epoch_i > epoch + 300:
                             self.checkpoint(epoch, experiment.curr_step, True)
 
             if end_flag:
