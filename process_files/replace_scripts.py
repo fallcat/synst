e5ae29bc6e2eb91e8d5312a003ebccc126cfc187
fallcat <rachel_you@outlook.com>
2020-03-22 21:24:20 -0400

    change scripts

--- process_files/replace_scripts.py
+++ process_files/replace_scripts.py
@@ -45,4 +45,5 @@ for filename in os.listdir(experiment_folder):
                 text = text.replace('--job-name=', '--job-name=04t')
                 # text = text.replace('small_enro_rl04', 'small_enro_rl05')
                 text = text.replace('2080ti-short', '1080ti-long')
+                text = text.replace('--split valid', '--split test')
                 output_file.write(text)
