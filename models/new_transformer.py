fbfd74185ea22a706743676c9e6d49376d7162df
fallcat <rachel_you@outlook.com>
2020-03-29 18:22:28 -0400

    pdb

--- models/new_transformer.py
+++ models/new_transformer.py
@@ -256,7 +256,12 @@ class TransformerDecoderLayer(nn.Module):
                 try:
                     # pdb.set_trace()
                     cache[self.uuid] = torch.cat((cached, state_to_cache), 1)
-                    state = torch.cat((cached.unsqueeze(1).expand(-1, self.layermasks_len, state.shape[1], state.shape[2]), state), 1)
+                    state = torch.cat((cached.unsqueeze(1).expand(-1,
+                                                                  self.layermasks_len,
+                                                                  state.shape[1],
+                                                                  state.shape[2]).view(-1,
+                                                                                       state.shape[1],
+                                                                                       state.shape[2]), state), 1)
                 except:
                     pdb.set_trace()
 
