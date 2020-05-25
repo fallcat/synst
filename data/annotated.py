b4d4185a5d183176035a1ce132fcc92a8a0a0109
fallcat <rachel_you@outlook.com>
2020-05-25 16:46:54 -0400

    pdb

--- data/annotated.py
+++ data/annotated.py
@@ -305,7 +305,7 @@ class AnnotatedTextDataset(TextDataset):
 
     def preprocess(self):
         ''' Do any data preprocessing if needed '''
-        #pdb.set_trace()
+        pdb.set_trace()
         if (
                 all(os.path.exists(p) for p in self.data_paths) and
                 all(os.path.exists(p) for p in self.vocab_paths)
